21d362f06ba148b4e8cf4bb95d2dcb057016b4e8
Genki Yamauchi <yamauchi-g573bs@pwri.go.jp>
2022-11-22 16:46:05 +0200

    add some msgs for excavator_com3

--- excavator_com3_struct.hpp
+++ excavator_com3_struct.hpp
@@ -8,6 +8,30 @@
 
 namespace excavator_com3
 {
+  struct machine_state
+  {
+    std::uint8_t alive_counter;
+    static const std::uint64_t id = 2550202115;
+    static const std::uint64_t cycle_time = 100;
+    std::uint64_t Id() { return id; }
+    std::uint64_t Cycle_time() { return cycle_time; }
+    RTTR_ENABLE()
+  };
+  struct machine_setting_cmd
+  {
+    bool travel_mode;
+    bool power_eco_mode;
+    bool hydraulic_onoff;
+    std::uint8_t engine_rpm;
+    bool engine_onoff;
+    std::uint8_t alive_counter;
+    static const std::uint64_t id = 2550202114;
+    static const std::uint64_t cycle_time = 100;
+    std::uint64_t Id() { return id; }
+    std::uint64_t Cycle_time() { return cycle_time; }
+    RTTR_ENABLE()
+  };
+
   struct lever_cmd_2
   {
     bool right_track_direction_cmd;
@@ -42,13 +66,26 @@ namespace excavator_com3
     std::uint64_t Cycle_time() { return cycle_time; }
     RTTR_ENABLE()
   };
-  struct Can_Bus : lever_cmd_2, lever_cmd_1
+  struct Can_Bus : machine_state, machine_setting_cmd, lever_cmd_2, lever_cmd_1
   {
-    RTTR_ENABLE(lever_cmd_2, lever_cmd_1)
+    RTTR_ENABLE(machine_state, machine_setting_cmd, lever_cmd_2, lever_cmd_1)
   };
 
   RTTR_REGISTRATION
   {
+    rttr::registration::class_<machine_state>("machine_state")
+        .constructor<>()
+        .property("machine_state::alive_counter", &machine_state::alive_counter);
+
+    rttr::registration::class_<machine_setting_cmd>("machine_setting_cmd")
+        .constructor<>()
+        .property("machine_setting_cmd::travel_mode", &machine_setting_cmd::travel_mode)
+        .property("machine_setting_cmd::power_eco_mode", &machine_setting_cmd::power_eco_mode)
+        .property("machine_setting_cmd::hydraulic_onoff", &machine_setting_cmd::hydraulic_onoff)
+        .property("machine_setting_cmd::engine_rpm", &machine_setting_cmd::engine_rpm)
+        .property("machine_setting_cmd::engine_onoff", &machine_setting_cmd::engine_onoff)
+        .property("machine_setting_cmd::alive_counter", &machine_setting_cmd::alive_counter);
+
     rttr::registration::class_<lever_cmd_2>("lever_cmd_2")
       .constructor<>()
       .property("lever_cmd_2::right_track_direction_cmd", &lever_cmd_2::right_track_direction_cmd)
